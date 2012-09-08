b917159f5d615d49f0107bfec6ece6ef80dee930
Adam Honoré <Adam.Honore@Gmail.com>
2012-09-08 18:11:58 +0200

    register with the affiliate server after location has been fetched

--- src/dk/pizzapp/android/activity/SplashscreenLocationListener.java
+++ src/dk/pizzapp/android/activity/SplashscreenLocationListener.java
@@ -1,20 +1,29 @@
 package dk.pizzapp.android.activity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.*;
 import android.os.Bundle;
+import android.provider.Settings;
+import com.androidquery.AQuery;
+import com.androidquery.callback.AjaxCallback;
+import com.androidquery.callback.AjaxStatus;
 import dk.pizzapp.android.App;
+import org.json.JSONObject;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 public class SplashscreenLocationListener implements LocationListener {
-    private Context context;
+    private Activity activity;
     private LocationManager locationManager;
 
-    public SplashscreenLocationListener(Context context, LocationManager locationManager) {
-        this.context = context;
+    public SplashscreenLocationListener(Activity activity, LocationManager locationManager) {
+        this.activity = activity;
         this.locationManager = locationManager;
     }
 
@@ -26,21 +35,54 @@ public class SplashscreenLocationListener implements LocationListener {
             locationManager.removeUpdates(this);
 
             // Convert the location to an address using the Geocoder API
-            Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+            Geocoder geocoder = new Geocoder(activity, Locale.getDefault());
             List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
 
             // Set the location and address in the application class
             App.location = location;
             App.address = addressList.get(0);
 
-            // Finish the splashscreen and continue to the main activity
-            context.startActivity(new Intent(context, Main.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            // Register the device with the server
+            if (!isDeviceRegistered())
+                registerDevice();
         }
 
         // Continue looking for a location if we got an error parsing the current one
         catch (Exception e) {
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 500, 1000, this);
         }
+    }
+
+    private void registerDevice() {
+
+        // Instantiate android-query and register with the server
+        AQuery aQuery = new AQuery(activity);
+        String url = "http://affiliate.pizzapp.dk/register.php";
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("uuid", Settings.Secure.getString(activity.getContentResolver(), Settings.Secure.ANDROID_ID));
+        params.put("url", "new");
+        params.put("type", "android");
+
+        aQuery.ajax(url, params, JSONObject.class, new AjaxCallback<JSONObject>() {
+            @Override
+            public void callback(String url, JSONObject object, AjaxStatus status) {
+
+                // Set device as registered
+                SharedPreferences.Editor editor = activity.getSharedPreferences("PizzApp", Context.MODE_PRIVATE).edit();
+                editor.putBoolean("registered", true).commit();
+
+                // Finish the splashscreen and continue to the main activity
+                activity.startActivity(new Intent(activity, Main.class));
+                activity.finish();
+            }
+        });
+    }
+
+    private boolean isDeviceRegistered() {
+
+        // Check the registered state from the shared preferences
+        SharedPreferences preferences = activity.getSharedPreferences("PizzApp", Context.MODE_PRIVATE);
+        return preferences.getBoolean("registered", false);
     }
 
     @Override
